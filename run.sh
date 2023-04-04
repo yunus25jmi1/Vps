c26aeeadc283beab3cec43cd8cbc9b17a27afa8c
Yunus Ahmad <115855149+yunus25jmi1@users.noreply.github.com>
2023-04-04 12:09:09 +0530

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,13 @@
+if [ -n "${BASE_CONF}" ] && [ -n "${CLOUD_NAME}" ] ; then
+    pgrep rclone
+    if [ $? -eq 0 ]; then
+        echo "already mounred skipping"
+    else 
+        echo $BASE_CONF | base64 -d > .rclone.conf
+        rclone serve sftp "$CLOUD_NAME":$SUB_DIR --no-auth --vfs-cache-mode full&
+    fi
+        
+else 
+    echo "CLOUD NOT MOUNTED" > /home/coder/CLOUD_NOT_MOUNTED
+fi
+
